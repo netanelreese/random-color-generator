70c8aa5826a7420ae289cafbaf571f5f0d6ba2a0
Nathanael <ngreese7@ou.edu>
2021-03-09 11:41:42 -0600

    Removed extends JFrame and added documentation

--- src/Driver.java
+++ src/Driver.java
@@ -3,15 +3,15 @@ import java.awt.event.ActionEvent;
 import javax.swing.*;
 
 
-public class Driver extends JFrame {
+public class Driver {
 
 	public static void main(String[] args) {
-		App app = new App();
-		app.launch();
+		App app = new App(); //creating app instance
+		app.launch(); //launching the splash applicaton
 
-	    app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    app.pack();
-	    app.setVisible(true);
+	    app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //terminates program on close
+	    app.pack(); //making program look good
+	    app.setVisible(true); //setting application visible
 	}
 
 }
